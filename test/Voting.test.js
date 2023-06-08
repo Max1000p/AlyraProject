95c8f9d1729e6bd7327390f9d66516a8c5899509
Cyril PAREJA <pareja@MacBook-Air-de-Cyril.local>
2023-06-09 00:00:13 +0200

    Unit test

--- test/Voting.test.js
+++ test/Voting.test.js
@@ -1,54 +1,25 @@
-const Spa = artifacts.require("./Spa.sol");
+const Voting = artifacts.require("./Voting.sol");
 const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
 
-contract('Spa', accounts => {
+contract('Voting', accounts => {
     const owner = accounts[0];
     const second = accounts[1];
     const third = accounts[2];
 
-    let SpaInstance;
+    let VotingInstance;
 
-    describe.skip("test setter/getter", function () {
+    describe("Test Workflowstatus and Registration Voters", function () {
 
         beforeEach(async function () {
-            SpaInstance = await Spa.new({from:owner});
+            VotingInstance = await Voting.new({from:owner});
         });
 
-        it("should store animal in array, get race", async () => {
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            const storedData = await SpaInstance.Get(0);
-            expect(storedData.race).to.equal("chien");
+        it("should Workflow status is set to Registering Voters", async () => {
+            expect(await VotingInstance.workflowStatus.call()).to.be.bignumber.equal(new BN(0));
         });
 
-        it("should store animal in array, get height", async () => {
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            const storedData = await SpaInstance.Get(0);
-            expect(storedData.taille).to.be.bignumber.equal(new BN(100));
-        });
-        it("should store animal in array, get age", async () => {
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            const storedData = await SpaInstance.Get(0);
-            expect(storedData.age).to.be.bignumber.equal(new BN(3));
-        });
-        it("should store animal in array, get status", async () => {
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            const storedData = await SpaInstance.Get(0);
-            expect(storedData.isAdopted).to.be.false;
-        });
-        it("should store 10 animals in array, get 1 height", async () => {
-            await SpaInstance.Add("chien",100, 3, { from: owner });            
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            await SpaInstance.Add("chien",150, 4, { from: owner });
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            await SpaInstance.Add("chien",100, 3, { from: owner });
-            const storedData = await SpaInstance.Get(4);
-            expect(storedData.taille).to.be.bignumber.equal(new BN(150));
-        });
+       
+       
     });
   })
